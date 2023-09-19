6bce02abe34f66962b5b4b8c13c2efa8fc69ad45
tneils218 <trantrungnhat2512@gmail.com>
2023-09-19 16:56:47 +0700

    add api addmoney, withdraw money

---
+++ WalletEcom/Controllers/Request/WalletAddOrWithdrawMoneyRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace WalletEcom.Controllers.Request
+{
+    public class WalletAddOrWithdrawMoneyRequest
+    {
+
+        public int Id { get; set; }
+        public int WalletId { get; set; }
+
+        public decimal Amount { get; set; }
+        public int ActionTypeId { get; set; }
+    }
+}
--- WalletEcom/Services/DTOs/WalletQueueDataDTO.cs
+++ WalletEcom/Services/DTOs/WalletQueueDataDTO.cs
@@ -2,27 +2,40 @@
 {
     public class WalletQueueDataDTO
     {
+
         public int ActionId { get; set; }
         public int WalletId { get; set; }
         public decimal Amount { get; set; }
-        public int? DestinationId { get; set; }
-        public int? DestinationWalletID { get; set; }
+        public int DestinationId { get; set; }
+        public int DestinationWalletID { get; set; }
         public int SourceId { get; set; }
 
-        private WalletQueueDataDTO(int actionID, int walletId, decimal amount, int sourceId, int? destinationId, int? destinationWalletID)
+        private WalletQueueDataDTO(int actionID, int walletId, decimal amount, int sourceId, int destinationId, int destinationWalletID)
         {
             ActionId = actionID;
             WalletId = walletId;
             Amount = amount;
-            DestinationId = destinationId ?? 0;
+            DestinationId = destinationId;
             SourceId = sourceId;
-            DestinationWalletID = destinationWalletID ?? 0;
+            DestinationWalletID = destinationWalletID;
         }
 
+        private WalletQueueDataDTO(int walletId, decimal amount, int actionID)
+        {
+            WalletId = walletId;
+            Amount = amount;
+            ActionId = actionID;
 
-        public static WalletQueueDataDTO Create(int actionID, int walletId, decimal amount, int sourceId, int? destinationId, int? destinationWalletId)
+        }
+
+        public static WalletQueueDataDTO Create(int actionID, int walletId, decimal amount, int sourceId, int destinationId, int destinationWalletId)
         {
             return new WalletQueueDataDTO(actionID, walletId, amount, sourceId, destinationId, destinationWalletId);
         }
+        public static WalletQueueDataDTO CreateForAdd(int walletId, decimal amount, int actionTypeId)
+        {
+            return new WalletQueueDataDTO(walletId, amount, actionTypeId);
+        }
+
     }
 }
